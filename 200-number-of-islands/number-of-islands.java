432a2138a6e41faf765759064890ee49cf0679a8
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2026-01-03 13:44:53 +0530

    Time: 3 ms (86.86%) | Memory: 51.8 MB (77.71%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -54,7 +54,7 @@ class Solution {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(visited[i][j]!=1 && grid[i][j]=='1'){
-                    System.out.println(i+" "+j);
+                    // System.out.println(i+" "+j);
                     count++;
                     fn(i,j,visited,grid);
                 }
