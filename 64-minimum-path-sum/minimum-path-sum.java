c757cb0caf78cac1778cb93176787d48d0b5a110
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2026-03-21 13:53:04 +0530

    Time: 1 ms (99.73%) | Memory: 50 MB (92.81%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,43 +1,28 @@
 class Solution {
     int dp[][];
-    public int fn(int i,int j,int grid[][]){
-        if(i==0&&j==0){
-            return grid[0][0];
+    public int fn(int i,int j,int grid[][],int m,int n){
+        if(i==m-1 && j==n-1){
+            return grid[i][j];
         }
-        int up;
-        if(i>0)  up=grid[i][j]+fn(i-1,j,grid);
-        else up=Integer.MAX_VALUE;
-        int left;
-        if(j>0) left=grid[i][j]+fn(i,j-1,grid);
-        else left=Integer.MAX_VALUE;
 
-        return Math.min(up,left);
+        if(i>=m || j>=n){
+            return (int)1e9;
+        }
+         if (dp[i][j] != -1) return dp[i][j];
+
+        int down=grid[i][j]+fn(i+1,j,grid,m,n);
+        int right=grid[i][j]+fn(i,j+1,grid,m,n);
+
+        return dp[i][j]=Math.min(down,right);
     }
+
     public int minPathSum(int[][] grid) {
-        
         int m=grid.length;
         int n=grid[0].length;
         dp=new int[m][n];
-        // return fn(m-1,n-1,grid);
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=grid[0][0];
-                }
-                else{
-                    int up;
-                    if(i>0)  up=grid[i][j]+dp[i-1][j];
-                    else up=Integer.MAX_VALUE;
-                    int left;
-                    if(j>0) left=grid[i][j]+dp[i][j-1];
-                    else left=Integer.MAX_VALUE;
-
-                    dp[i][j]=Math.min(up,left);
-                }
-            }
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-
-        return dp[m-1][n-1];
+       return  fn(0,0,grid,m,n);
     }
 }
