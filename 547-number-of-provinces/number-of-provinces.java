4292857f55a8d70e26726b92aec9756f18d70092
ShivaKoushik <83773902+ShivaKoushik33@users.noreply.github.com>
2026-03-28 14:50:10 +0530

    Time: 4 ms (18.18%) | Memory: 49.5 MB (13.63%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,50 +1,38 @@
 class Solution {
-    int visited[];
-    int count;
-    public void fn(int Node,int visited[], List<List<Integer>> l){
-        visited[Node]=1;
-        for(int x:l.get(Node-1)){
-            if(visited[x]!=1){
-                 fn(x,visited,l);
+    void fn( List<List<Integer>> l,int ind,int visited[]){
+        visited[ind]=1;
+        for(int x:l.get(ind-1)){
+            if(visited[x]==-1){
+                fn(l,x,visited);
             }
-           
         }
     }
     public int findCircleNum(int[][] isConnected) {
+        int count=0;
         int n=isConnected.length;
-        count=0;
-        visited=new int[n+1];
+        int visited[]=new int[n+1];
+        Arrays.fill(visited,-1);
         List<List<Integer>> l=new ArrayList<>();
         for(int i=0;i<n;i++){
             List<Integer> temp=new ArrayList<>();
             for(int j=0;j<n;j++){
                 if(i!=j){
                     if(isConnected[i][j]==1){
-                            temp.add(j+1);
+                        temp.add(j+1);
                     }
                 }
             }
-            l.add((temp));
+            l.add(new ArrayList<>(temp));
         }
 
-        // for (List<Integer> innerList :l) {
-        //     for (Integer val : innerList) {
-        //         System.out.print(val + " ");
-        //     }
-        //     System.out.println();
-        // }
+
 
         for(int i=1;i<=n;i++){
-                if(visited[i]!=1){
-                    count++;
-                    fn(i,visited,l);
-                }
+            if(visited[i]!=1){
+                fn(l,i,visited);
+                count++;
+            }
         }
-
-
-
-
         return count;
-
     }
 }
